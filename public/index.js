a5683f5f6a740ed6834ce811292a4bb3c51a061c
jinho choi <calmglow@gmail.com>
2022-01-09 01:43:29 +0900

    stat정보 추가

--- public/index.js
+++ public/index.js
@@ -3,12 +3,43 @@ const addButton= document.getElementById("addButton")
 const currentCountBox= document.getElementById("currentCount")
 const audioBar= document.getElementById("audioBar")
 const resetButton= document.getElementById("resetTodayButton")
+const statButton= document.getElementById("statButton")
+const statDiv= document.getElementById("statDiv")
 const store= window.localStorage
-const d= new Date()
-const today= (d.getYear() + 1900) + "-" + (d.getMonth() + 1) + "-" + d.getDate()
+const today= new Date()
+const 지난달= new Date()
+지난달.setMonth(지난달.getMonth()-1)
+const 지지난달= new Date()
+지지난달.setMonth(지지난달.getMonth()-2)
+const todayText= (today.getYear() + 1900) + "-" + (today.getMonth() + 1) + "-" + today.getDate()
+const 이번달Text= (today.getYear() + 1900) + "-" + (today.getMonth() +1)
+const 지난달Text= (지난달.getYear() + 1900) + "-" + (지난달.getMonth() +1)
 let countList= []
+let 지난달Stat={
+  명상한날수:0,
+  일별평균숨수:0,
+  일별기록:[]
+}
+let 이번달Stat={
+  명상한날수:0,
+  일별평균숨수:0,
+  일별기록:[]
+}
 initStore(store)
 showCount(count)
+이번달정보가져오기()
+지난달정보가져오기()
+showStat()
+function showStat(){
+  let statText= `<h2>이번달</h2><p>명상한날수: ${이번달Stat.명상한날수}</p>
+  <p>평균숨수: ${이번달Stat.일별평균숨수}</p>
+  <h2>지난달</h2><p>명상한날수: ${지난달Stat.명상한날수}</p>
+  <p>평균숨수: ${지난달Stat.일별평균숨수}</p>
+  <h2>세부기록</h2>
+  `
+
+  statDiv.innerHTML=statText
+}
 addButton.addEventListener("click", async () =>{
   increase()
   if (countList.length %10 == 0) await audioBar.play()
@@ -18,25 +49,63 @@ resetButton.addEventListener("click", async () =>{
   resetCount()
   showCount()
 })
+statButton.addEventListener("click", () => {
+  const modal= document.querySelector(".modal")
+  if(modal.style.display === 'block') {
+    modal.style.display= 'none'
+    document.querySelector("body").style.overflow= 'auto'
+  }else {
+    modal.style.display = 'block'
+    document.querySelector("body").style.overflow= 'hidden'
+  }
+  console.log(modal.style.display)
+},false)
 function increase(){
   countList.push(new Date().getTime())
-  store.setItem(today, JSON.stringify(countList))
+  store.setItem(todayText, JSON.stringify(countList))
+}
+function 지난달정보가져오기(){
+  let keyDate=0;
+  let 숨수=0;
+  for (i = 0; i < store.length; i++){
+    keyDate= store.key(i)
+    if (keyDate.startsWith(지난달Text)){
+      지난달Stat.명상한날수++
+      숨수= 숨수 + JSON.parse(store.getItem(keyDate)).length
+      지난달Stat.일별기록.push({keyDate:JSON.parse(store.getItem(keyDate))})
+    }
+  }
+  지난달Stat.일별평균숨수= 숨수/지난달Stat.명상한날수
+  
+}
+function 이번달정보가져오기(){
+  let keyDate=0;
+  let 숨수=0;
+  for (i = 0; i < store.length; i++){
+    keyDate= store.key(i)
+    if (keyDate.startsWith(이번달Text)){
+      이번달Stat.명상한날수++
+      숨수= 숨수 + JSON.parse(store.getItem(keyDate)).length
+      이번달Stat.일별기록.push({keyDate:JSON.parse(store.getItem(keyDate))})
+    }
+  }
+  이번달Stat.일별평균숨수= 숨수/이번달Stat.명상한날수
 }
 function decrease(){
   countList.pop()
-  store.setItem(today, JSON.stringify(countList))
+  store.setItem(todayText, JSON.stringify(countList))
 }
 function resetCount(){
   countList = []
-  store.setItem(today, countList)
+  store.setItem(todayText, countList)
 }
 function showCount(){
   currentCountBox.innerHTML=countList.length
 }
 function initStore(s){
-  if (s.getItem(today) !== null && s.getItem(today) !== ''){
-    countList= JSON.parse(s.getItem(today))
+  if (s.getItem(todayText) !== null && s.getItem(todayText) !== ''){
+    countList= JSON.parse(s.getItem(todayText))
   }else{
-    s.setItem(today, JSON.stringify(countList))
+    s.setItem(todayText, JSON.stringify(countList))
   }
 }
